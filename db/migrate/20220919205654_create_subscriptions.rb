b6374ffb7734a80f3826aa3def73b51d19d53ccc
Clay Ash <claycourtneyash@gmail.com>
2022-09-20 14:27:24 -0600

    Change subscription status default to active

--- db/migrate/20220919205654_create_subscriptions.rb
+++ db/migrate/20220919205654_create_subscriptions.rb
@@ -5,7 +5,7 @@ class CreateSubscriptions < ActiveRecord::Migration[5.2]
       t.references :customer, foreign_key: true
       t.string :title 
       t.float :price 
-      t.string :status, default: "Inactive"
+      t.string :status, default: "Active"
       t.string :frequency
     end
   end
